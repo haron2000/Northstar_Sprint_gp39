443d8251da55c91dcbe4f236010c6216353d937f
HaronMwilu <75951017+haron2000@users.noreply.github.com>
2026-08-14 09:40:46 +0300

    feat: add Product and ProductVariant models - stores stock data per product and size

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ProductVariant
+
+
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'sku', 'category')
+    search_fields = ('name', 'sku')
+    inlines = [ProductVariantInline]
+
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = ('product', 'size', 'stock_quantity')
+    list_filter = ('product',)
